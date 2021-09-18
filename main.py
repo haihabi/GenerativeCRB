a45def3ed68f26f5eaac78eaa62318c144da1711
HVH <haihabi1986@gmail.com>
2021-09-19 01:00:25 +0300

    Update main.py
    Change network generation function

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def config():
     cr.add_parameter('dim', default=16, type=int)
     cr.add_parameter('theta_min', default=-10, type=float)
     cr.add_parameter('theta_max', default=10.0, type=float)
-    cr.add_parameter('sigma_n', default=1.0, type=float)
+    cr.add_parameter('sigma_n', default=10.0, type=float)
     ############################################
     # Regression Network
     #############################################
@@ -128,21 +128,34 @@ def generate_gcrb_validation_function(current_data_model, in_regression_network,
     # plt.show()
 
 
-def generate_flow_model(in_param, condition_embedding_size=1, n_layer_cond=4):
-    n_flow_blocks = in_param.n_flow_blocks
-    nfs_flow = nf.NSF_CL if True else nf.NSF_AR
-    flows = [nfs_flow(dim=in_param.dim, K=8, B=3, hidden_dim=16) for _ in range(n_flow_blocks)]
+def generate_flow_model(in_param, condition_embedding_size=1, n_layer_cond=4, spline_b=3, spline_k=8):
+    # n_flow_blocks = in_param.n_flow_blocks
+    # nfs_flow = nf.NSF_CL if True else nf.NSF_AR
+    # flows = [nfs_flow(dim=in_param.dim, K=8, B=3, hidden_dim=16) for _ in range(n_flow_blocks)]
     # flows = [MAF(dim=2, parity=i%2) for i in range(4)]
-    convs = [nf.Invertible1x1Conv(dim=in_param.dim) for _ in flows]
-    norms = [nf.BatchNorm(in_param.dim) for _ in flows]
-    affine = [
-        nf.ConditionalAffineHalfFlow(dim=in_param.dim, parity=i % 2, scale=True) for i, _ in enumerate(flows)]
-    affine_inj = [
-        nf.AffineInjector(dim=in_param.dim, net_class=nf.generate_mlp_class(24, n_layer=n_layer_cond), scale=True,
-                          condition_vector_size=condition_embedding_size) for
-        i, _ in enumerate(flows)]
+    # convs = [nf.Invertible1x1Conv(dim=in_param.dim) for _ in range(n_flow_blocks)]
+    # norms = [nf.BatchNorm(in_param.dim) for _ in range(n_flow_blocks)]
+    # affine = [
+    #     nf.ConditionalAffineHalfFlow(dim=in_param.dim, parity=i % 2, scale=True) for i in range(n_flow_blocks)]
+    # affine_inj = [
+    #     nf.AffineInjector(dim=in_param.dim, net_class=nf.generate_mlp_class(24, n_layer=n_layer_cond), scale=True,
+    #                       condition_vector_size=condition_embedding_size) for
+    #     _ in range(n_flow_blocks)]
+    flows = []
+    affine_coupling = False
+    spline_flow = False
+    for i in range(in_param.n_flow_blocks):
+        if affine_coupling:
+            flows.append(
+                nf.ConditionalAffineHalfFlow(dim=in_param.dim, parity=i % 2, scale=True))
+        flows.append(
+            nf.AffineInjector(dim=in_param.dim, net_class=nf.generate_mlp_class(24, n_layer=n_layer_cond), scale=True,
+                              condition_vector_size=condition_embedding_size))
 
-    flows = [*list(itertools.chain(*zip(affine_inj, convs)))]
+        flows.append(
+            nf.Invertible1x1Conv(dim=in_param.dim))
+        if spline_flow:
+            flows.append(nf.NSF_CL(dim=in_param.dim, K=spline_k, B=spline_b, hidden_dim=16))
 
     return nf.NormalizingFlowModel(MultivariateNormal(torch.zeros(in_param.dim, device=constants.DEVICE),
                                                       torch.eye(in_param.dim, device=constants.DEVICE)), flows,
