7914c960a9b773f6171149b05a94975a8d88fa9a
HVH <haihabi1986@gmail.com>
2021-10-15 14:07:03 +0300

    Bug fix sampling alg

--- gcrb/compuate_fim.py
+++ gcrb/compuate_fim.py
@@ -26,7 +26,7 @@ def compute_fim_tensor(in_model, in_theta_tensor, batch_size=128):
                                                 device=constants.DEVICE)
     nll_tensor = in_model.sample_nll(batch_size, cond=theta_tensor).reshape([-1, 1])
     j_matrix = jacobian_single(nll_tensor, theta_tensor)
-    return torch.matmul(j_matrix.transpose(dim0=1, dim1=2),j_matrix)
+    return torch.matmul(j_matrix.transpose(dim0=1, dim1=2), j_matrix)
 
 
 def compute_fim(in_model, in_theta_tensor, batch_size=128):
@@ -38,16 +38,16 @@ def adaptive_sampling_gfim(in_model, in_theta_tensor, batch_size=128, eps=0.01, 
     status = True
     iteration_step = 1
     while status:
-        for _ in tqdm(range(iteration_step)):
+        for _ in range(iteration_step):
             fim_list.append(compute_fim_tensor(in_model, in_theta_tensor, batch_size=batch_size))
         fim_stack = torch.cat(fim_list, dim=0)
 
         fim_stack = fim_stack[torch.logical_not(torch.any(torch.any(fim_stack.isnan(), dim=2), dim=1)),
                     :]  # Clear non values
 
-        var_mean_ratio = fim_stack.var(dim=0).max() / (torch.pow(fim_stack.mean(dim=0).max(), 2.0) + 1e-6)
+        var_mean_ratio = fim_stack.var(dim=0) / (torch.pow(fim_stack.mean(dim=0), 2.0) + 1e-6)
+        var_mean_ratio = var_mean_ratio.max()
         n_est = int(torch.ceil(var_mean_ratio / (p_min * (eps ** 2))).item())
-        print(100 * fim_stack.shape[0] / n_est)
         if n_est > fim_stack.shape[0] and fim_stack.shape[0] < n_max:
             iteration_step = min(math.ceil((n_est - fim_stack.shape[0]) / batch_size), 1000)
         else:
