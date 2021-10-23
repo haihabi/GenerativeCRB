bug fix in case of no gradient norm adding pspline flow

neural_network/training/single_network_optimization.py
@@ -46,7 +46,7 @@ class SingleNetworkOptimization(object):
             grad_norm = torch.norm(
                 torch.stack(
                     [torch.norm(p.grad.detach(), self.norm_type).to(constants.DEVICE) for p in
-                     self.network.parameters()]),
+                     self.network.parameters() if p.grad is not None]),
                 self.norm_type)
         self.opt.step()
         return grad_norm

normalizing_flow/flows/pspline_flows.py
@@ -29,7 +29,8 @@ class PNSF(nn.Module):
         log_det = torch.zeros(x.shape[0], device=constants.DEVICE)
         lower, upper = x[:, :self.dim // 2], x[:, self.dim // 2:]
         # out = self.f1(lower).reshape(-1, self.dim // 2, 3 * self.K - 1)
-        W, H, D = torch.split(self.p1, self.K, dim=2)
+        w1 = self.p1.reshape([1, -1]).repeat([x.shape[0], 1]).reshape(-1, self.dim // 2, 3 * self.K - 1)
+        W, H, D = torch.split(w1, self.K, dim=2)
         W, H = torch.softmax(W, dim=2), torch.softmax(H, dim=2)
         W, H = 2 * self.B * W, 2 * self.B * H
         D = F.softplus(D)
@@ -37,7 +38,9 @@ class PNSF(nn.Module):
         log_det += torch.sum(ld, dim=1)
 
         # out = self.f2(upper).reshape(-1, self.dim // 2, 3 * self.K - 1)
-        W, H, D = torch.split(self.p2, self.K, dim=2)
+        w2 = self.p2.reshape([1, -1]).repeat([x.shape[0], 1]).reshape(-1, self.dim // 2, 3 * self.K - 1)
+
+        W, H, D = torch.split(w2, self.K, dim=2)
         W, H = torch.softmax(W, dim=2), torch.softmax(H, dim=2)
         W, H = 2 * self.B * W, 2 * self.B * H
         D = F.softplus(D)
@@ -49,15 +52,17 @@ class PNSF(nn.Module):
     def backward(self, z, cond=None):
         log_det = torch.zeros(z.shape[0], device=constants.DEVICE)
         lower, upper = z[:, :self.dim // 2], z[:, self.dim // 2:]
+        w2 = self.p2.reshape([1, -1]).repeat([z.shape[0], 1]).reshape(-1, self.dim // 2, 3 * self.K - 1)
 
-        W, H, D = torch.split(self.p2, self.K, dim=2)
+        W, H, D = torch.split(w2, self.K, dim=2)
         W, H = torch.softmax(W, dim=2), torch.softmax(H, dim=2)
         W, H = 2 * self.B * W, 2 * self.B * H
         D = F.softplus(D)
         lower, ld = unconstrained_RQS(lower, W, H, D, inverse=True, tail_bound=self.B)
         log_det += torch.sum(ld, dim=1)
+        w1 = self.p1.reshape([1, -1]).repeat([z.shape[0], 1]).reshape(-1, self.dim // 2, 3 * self.K - 1)
 
-        W, H, D = torch.split(self.p1, self.K, dim=2)
+        W, H, D = torch.split(w1, self.K, dim=2)
         W, H = torch.softmax(W, dim=2), torch.softmax(H, dim=2)
         W, H = 2 * self.B * W, 2 * self.B * H
         D = F.softplus(D)